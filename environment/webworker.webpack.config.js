d8e357c59c9acef513b59b5de227b1eec6a49d86
Artem Nikolaev <artem@nikolaev.by>
2022-02-12 01:52:06 +0300

    release: v0.1.0

--- environment/webworker.webpack.config.js
+++ environment/webworker.webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     entry: './_/api-worker/index.js',
     output: {
         path: path.resolve(__dirname, '../dist'),
