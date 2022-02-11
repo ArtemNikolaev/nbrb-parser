82b2b543df8c8adfd38841de7674d30860990d26
Artem Nikolaev <artem@nikolaev.by>
2022-02-11 23:55:57 +0300

    move rates logic to controller

--- _/worker/API/api.js
+++ _/worker/API/api.js
@@ -2,13 +2,5 @@ const baseUrl = 'https://www.nbrb.by/api/'
 
 module.exports = {
     CURRENCIES: `${baseUrl}exrates/currencies`,
-    RATES: (id, periodicity = 0, ondate, parammode =2) => {
-        const url = new URL(`${baseUrl}exrates/rates${id ? `/${id}`: ''}`);
-
-        if (Number.isInteger(periodicity)) url.searchParams.set('periodicity', periodicity);
-        if (ondate) url.searchParams.set('ondate', ondate);
-        if (Number.isInteger(parammode)) url.searchParams.set('parammode', parammode);
-
-        return url.toString();
-    }
+    RATES: `${baseUrl}exrates/rates`,
 }
--- _/worker/API/getRates.js
+++ _/worker/API/getRates.js
@@ -2,7 +2,14 @@ const fetchJSON = require('../utils/fetchJSON');
 const { RATES } = require('./api');
 
 module.exports =
-    (id, ondate, periodicity, parammode) =>
-        fetchJSON(
-            RATES(id, ondate, periodicity, parammode)
-        );
+    (id, ondate, periodicity = 0, parammode = 2) => {
+            const url = new URL(
+                `${RATES}${id ? `/${id}`: ''}`
+            );
+
+            if (Number.isInteger(periodicity)) url.searchParams.set('periodicity', periodicity);
+            if (ondate) url.searchParams.set('ondate', ondate);
+            if (Number.isInteger(parammode)) url.searchParams.set('parammode', parammode);
+
+            return fetchJSON(url.toString());
+    }
